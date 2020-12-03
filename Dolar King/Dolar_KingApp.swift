263292628aef17847eaa5f03f8276324786dd08a
Carlos Caraccia <carloscaraccia@gmail.com>
2020-12-03 16:48:56 -0300

    Initial Commit

---
+++ Dolar King/Dolar_KingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dolar_KingApp.swift
+//  Dolar King
+//
+//  Created by Carlos Caraccia on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct Dolar_KingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
