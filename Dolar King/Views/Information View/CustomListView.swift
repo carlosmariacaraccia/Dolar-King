e750880d18c296fcd89ff2b315f3269ce01c967a
Carlos Maria Caraccia <carloscaraccia@gmail.com>
2020-12-17 09:42:22 -0300

    added the pull to refresh functionalith

---
+++ Dolar King/Views/Information View/CustomListView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomListView.swift
+//  Dolar King
+//
+//  Created by Carlos Caraccia on 12/17/20.
+//
+
+import SwiftUI
+
+struct CustomListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomListView()
+    }
+}
---
+++ Dolar King/Views/Information View/CustomScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomScrollView.swift
+//  Dolar King
+//
+//  Created by Carlos Caraccia on 12/17/20.
+//
+
+import SwiftUI
+
+struct CustomScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomScrollView()
+    }
+}
